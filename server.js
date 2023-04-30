a46e786ae61a66aadc1c216d215e5343cb9e65e6
yawam <93334358+yawam@users.noreply.github.com>
2023-04-29 21:48:02 -0600

    finished front end

--- server.js
+++ server.js
@@ -6,13 +6,25 @@
  * Require Statements
  *************************/
 const express = require("express")
+const expressLayouts = require("express-ejs-layouts")
 const env = require("dotenv").config()
 const app = express()
+
+/**
+ * View Engine and Templates
+ */
+app.set("view engine", "ejs")
+app.use(expressLayouts)
+app.set("layout", "./layouts/layout") // not at view root
 
 /* ***********************
  * Routes
  *************************/
 app.use(require("./routes/static"))
+// Index route
+app.get("/", function(req, res){
+  res.render("index", {title: "Home"})
+})
 
 /* ***********************
  * Local Server Information
