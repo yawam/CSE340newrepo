a0de3eb70bab99c05fe2ab1c85a585c6b8e62c3f
yawam <93334358+yawam@users.noreply.github.com>
2023-05-24 23:14:51 -0600

    Team Activities done

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const express = require("express")
 const expressLayouts = require("express-ejs-layouts")
 const env = require("dotenv").config()
 const app = express()
-
+const utilities = require("./utilities/")
 
 /**
  * View Engine and Templates
@@ -24,9 +24,31 @@ app.set("layout", "./layouts/layout") // not at view root
  *************************/
 app.use(require("./routes/static"))
 // Index route
-app.get("/", baseController.buildHome)
+app.get("/", utilities.handleErrors(baseController.buildHome))
 // Inventory routes
 app.use("/inv", require("./routes/inventoryRoute"))
+// File Not Found Route - must be last route in list
+app.use(async (req, res, next) => {
+  next({status: 404, message: 'Sorry, we appear to have lost that page.'})
+})
+
+
+/* ***********************
+* Express Error Handler
+* Place after all other middleware
+*************************/
+app.use(async (err, req, res, next) => {
+  let nav = await utilities.getNav()
+  console.error(`Error at: "${req.originalUrl}": ${err.message}`)
+  if(err.status == 404){ message = err.message} else {message = 'Oh no! There was a crash. Maybe try a different route?'}
+  res.render("errors/error", {
+    title: err.status || 'Server Error',
+    message,
+    nav
+  })
+})
+
+
 
 /* ***********************
  * Local Server Information
