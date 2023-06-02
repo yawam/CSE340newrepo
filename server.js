9a4c265fc0f2592f450ed1c1548178c61e0942fc
yawam <93334358+yawam@users.noreply.github.com>
2023-06-01 23:45:17 -0600

    sessions

--- server.js
+++ server.js
@@ -11,6 +11,8 @@ const expressLayouts = require("express-ejs-layouts")
 const env = require("dotenv").config()
 const app = express()
 const utilities = require("./utilities/")
+const session = require("express-session")
+const pool = require("./database/")
 
 /**
  * View Engine and Templates
@@ -18,6 +20,21 @@ const utilities = require("./utilities/")
 app.set("view engine", "ejs")
 app.use(expressLayouts)
 app.set("layout", "./layouts/layout") // not at view root
+
+
+/***
+ * Middlewre
+ */
+app.use(session({
+  store: new (require('connect-pg-simple')(session))({
+    createTableIfMissing: true,
+    pool,
+  }),
+  secret: process.env.SESSION_SECRET,
+  resave: true,
+  saveUninitialized: true,
+  name: "sessionID",
+}))
 
 /* ***********************
  * Routes
